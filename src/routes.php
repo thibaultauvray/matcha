b94a785e7f4dbfc3a0a08fd5bb4f8f1d93dd9da1
Thibault AUVRAY <tauvray@student.42.fr>
2016-09-07 18:04:39 +0200

    Homepage time to doing real stuff

--- src/routes.php
+++ src/routes.php
@@ -4,12 +4,4 @@
 $router = new Router($app);
 
 $router->get('/mvc', 'Pages@index');
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-	$pdo = $this->db->prepare('SELECT * FROM users');
-	$pdo->execute();
-
-    // Render index view
-    return $this->view->render($response, 'index.html.twig', $args);
-})->setName('homepage');
+$router->get('/', 'Pages@index')->setName('homepage');
